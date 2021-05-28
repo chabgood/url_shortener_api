96b41e2bb89cadace278fa2e223c0b7385df55de
Chris Habgood <chabgood@gmail.com>
2021-05-28 02:00:13 -0500

    redirect spec works

--- spec/features/show_spec.rb
+++ spec/features/show_spec.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-# RSpec.feature 'Html Show', type: :feature do
-#   scenario 'redirects to the url' do
-#     url_shortener = FactoryBot.create(:url_shortener)
-#     visit api_v1_show_url(url_shortener.slug)
-#     expect(current_path).to eq url_shortener.url
-#   end
-# end
+describe 'Html Show', type: :request do
+  it 'redirects to the url' do
+    url_shortener = FactoryBot.create(:url_shortener)
+    get api_v1_show_url(url_shortener.slug)
+    expect(response).to redirect_to url_shortener.url
+  end
+end
